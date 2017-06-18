c6a9b93606c4aa8e117ad61a9b153b5b13fd639a
Sinkjaer <sinkjaermikkel@gmail.com>
2017-06-18 12:58:56 +0200

    Virker ikke som den skal

--- aggregate_measurements.py
+++ aggregate_measurements.py
@@ -2,44 +2,79 @@
 
 #slettes
 from dataload import load_measurements
-[tvec,data] = load_measurements("testdata1.csv", "forward fill")
+[tvec,data] = load_measurements("2008.csv", "forward fill")
 
 
 
-hour = ''
-day = ''
-month = 1
-priode = [hour, day, month]
 
-
+tvec_a = tvec
+data_a = data
 
 #def aggregate_measurements(tvec, data, period):
 while True:
-    month =int(input('Wich month do you want?:  '))
-    if (month < 1) or (month > 12):
-        print('Month {:d} does not exsist. Try again!'.format(month))
+    try:
+        month =input('Wich month do you want?:  ')
+        if month == 'all':
+            break
+        else:
+            month = int(month)
+
+
+        if (month < 1) or (month > 12):
+            print('Month {:d} does not exsist. Try again!'.format(month))
+            pass
+        elif any( tvec.month == month):
+            data_a = data_a[tvec_a.month ==month]
+            tvec_a = tvec_a[tvec_a.month == month]
+            break
+        elif month == 'all':
+            break
+        else: 
+            print('Month {:d} is not in the data . Try another month.'.format(month))
+            pass
+    except ValueError:
         pass
-    elif any( tvec.month == month):
-        data = data[tvec.month ==month]
-        tvec = tvec[tvec.month == month]
-        break
-    elif month == 'all':
-        break
-    else: 
-        print('Month {:d} is not in the data . Try another month.'.format(month))
+
+
+while True:
+    try:
+        day = input('Which day do you want?:  ')
+        if day == 'all':
+            break
+        else:
+            day = int(day)
+        if ( day < 1) or ( day > 31):
+            print('Day {:d} does not exsist. Try again!'.format(day))
+            pass
+        elif any ( tvec_a.day == day ):
+            data_a = data_a[tvec_a.day == day]
+            tvec_a = tvec_a[tvec_a.day == day]
+        else: 
+            print('Day {:d} is not in the data . Try another month.'.format(day))
+            pass
+    except ValueError:
         pass
 
 
-if day != '':
-    data = data[tvec.day ==day]
-    tvec = tvec[tvec.day == day]
 
-if hour !='':
-    data = data[tvec.hour == hour]
-    tvec = tvec[tvec.hour == hour]
+while True:
+    try:
+        hour = input('Which hour do you want?:  ')
+        if hour == 'all':
+            break
+        else:
+            hour = int(hour)
+        if ( hour < 0) or ( hour > 23):
+            print('Hour {:d} does not exsist. Try again!'.format(hour))
+            pass
+        elif any ( tvec_a.hour == hour ):
+            data_a = data_a[tvec_a.hour == hour]
+            tvec_a = tvec_a[tvec_a.hour == hour]
 
-
-
-any( tvec.month == 2)
+        else: 
+            print('Day {:d} is not in the data . Try another month.'.format(day))
+            pass
+    except ValueError:
+        pass
 
 #return (tvec_a, data_a)

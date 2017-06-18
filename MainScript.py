dfa629d6167cc5ee9cdd6c5913edc5a9e820a5d0
Sinkjaer <sinkjaermikkel@gmail.com>
2017-06-18 12:06:03 +0200

    :)

--- MainScript.py
+++ MainScript.py
@@ -9,11 +9,7 @@ while True:
     choice = displayMenu(menuItems)
 
     if choice == 1:
-
      
             
     if choice == 5:
         break
-=======
-
->>>>>>> 54875ebd13d25ea5795c37c46e070399858722d2

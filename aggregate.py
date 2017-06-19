d4719150fb0072c6f354c9f04520fa4943bee3ef
Sinkjaer <sinkjaermikkel@gmail.com>
2017-06-19 21:23:53 +0200

    Så virker den

--- aggregate.py
+++ aggregate.py
@@ -7,28 +7,25 @@ import pandas as pd
 import numpy as np
 
 [tvec,data] = load_measurements("2008.csv", "drop")
-priode = 'month'
+periode = 'month'
 
 
 
 tvec_a = []
+data_a = []
 
-
-if priode == 'month':
+if periode == 'month':
     tvec_a =[None]*12
     data_a = [None]*12
     for i in range (12):
         someData = data[tvec.month == i+1]
-        
-        
-        
         someTvec = tvec[tvec.month == i+1]
-                
-        someData = someData.sum(axis=0)
-        someData = np.array([someData])
-        data_a[i] = someData
-        a = pd.DataFrame(columns=[ 'Zone1','Zone2','Zone3','Zone4'])
-        a[i] = someData
+        data_a[i] = someData.sum(axis = 0)
+    data_a = pd.DataFrame(data = data_a, index = )
+#        someData = someData.sum(axis=0)
+#        data_a[i] = someData
+#        a = pd.DataFrame(columns=[ 'Zone1','Zone2','Zone3','Zone4'])
+#        a[i] = someData
         
 
-
+if per
